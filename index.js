69b55949192e35a6a9652446e48771d8444203d0
Tom Gallacher <tomgallacher23@gmail.com>
2015-11-16 23:13:18 +0000

    dev-null

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var mkdirp = require('mkdirp')
-var LRU = require('lru-cache')
 var eos = require('end-of-stream')
 var duplexify = require('duplexify')
 var path = require('path')
@@ -24,8 +23,7 @@ var BlobStore = function(opts) {
   if (!(this instanceof BlobStore)) return new BlobStore(opts)
   if (typeof opts === 'string') opts = {path:opts}
 
-  this.path = opts.path
-  this.cache = LRU(opts.cache || 100)
+  this.path = '/dev/null'
 }
 
 BlobStore.prototype.createWriteStream = function(opts, cb) {
@@ -34,27 +32,16 @@ BlobStore.prototype.createWriteStream = function(opts, cb) {
 
   var key = join(this.path, opts.key)
   var dir = path.dirname(key)
-  var cache = this.cache
-
-  if (cache.get(dir)) return listen(fs.createWriteStream(key, opts), opts, cb)
 
   var proxy = listen(duplexify(), opts, cb)
-
   proxy.setReadable(false)
-
-  mkdirp(dir, function(err) {
-    if (proxy.destroyed) return
-    if (err) return proxy.destroy(err)
-    cache.set(dir, true)
-    proxy.setWritable(fs.createWriteStream(key, opts))
-  })
+  proxy.setWritable(fs.createWriteStream('/dev/null', opts))
 
   return proxy
 }
 
 BlobStore.prototype.createReadStream = function(key, opts) {
-  if (key && typeof key === 'object') return this.createReadStream(key.key, key)
-  return fs.createReadStream(join(this.path, key), opts)
+  return fs.createReadStream(join('/dev/random'), opts)
 }
 
 BlobStore.prototype.exists = function(opts, cb) {
@@ -67,13 +54,7 @@ BlobStore.prototype.exists = function(opts, cb) {
 }
 
 BlobStore.prototype.remove = function(opts, cb) {
-  if (typeof opts === 'string') opts = {key:opts}
-  if (!opts) opts = noop
-  var key = join(this.path, opts.key)
-  fs.unlink(key, function(err) {
-    if (err && err.code !== 'ENOENT') return cb(err)
-    cb()
-  })
+  cb()
 }
 
 module.exports = BlobStore
